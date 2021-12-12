8bebdc438a838f8f513034fcce4bb32b51172bd4
tartakovskyi <vvt2001@ukr.net>
2021-12-12 20:26:48 +0200

    before auth

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 
 
 Route::group(['namespace' => 'Api'], function () {
+
+    Route::group(['namespace' => 'Auth'], function () {
+        Route::post('register', 'RegisterController');
+        Route::post('login', 'LoginController');
+        Route::post('logout', 'LogoutController')->middleware('auth:api');
+        Route::middleware('auth:api')->get('current', function (Request $request) {
+            return $request->user();
+        });
+    });
 
     Route::apiResources([
         'post' => PostController::class,
