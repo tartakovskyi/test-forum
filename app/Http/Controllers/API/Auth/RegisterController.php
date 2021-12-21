d21f02556f7e4a2fe094fc93cc07f9ffb992995b
Volodymyr Tartakovskyi <vvt2001@ukr.net>
2021-12-21 16:44:17 +0200

    register

--- app/Http/Controllers/API/Auth/RegisterController.php
+++ app/Http/Controllers/API/Auth/RegisterController.php
@@ -3,26 +3,29 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
-//use App\Http\Requests\Api\Auth\RegisterFormRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    public function __invoke(/*RegisterFormRequest*/ Request $request)
+    public function __invoke(Request $request)
     {
 
         $validated = $request->validate([
-            'name' => 'required',
+            'login' => 'required|unique:users',
             'email' => 'required|unique:users',
+            'phone' => 'required|numeric|unique:users',
             'password' => 'required'
         ]);
 
 
         $user = User::create(array_merge(
-            $request->only('name', 'email'),
-            ['password' => bcrypt($request->password)],
+            $request->only('login', 'email', 'phone', 'first_name', 'last_name'),
+            [
+                'password' => bcrypt($request->password),
+                'role_id' => 3
+            ],
         ));
 
         return response()->json([
